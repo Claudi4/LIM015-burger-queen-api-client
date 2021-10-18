d27888527901f82783ea717368fd26b0a7644273
Claudi4 <clau.ale.20a@gmail.com>
2021-10-18 10:48:49 -0500

    comments fixed

--- burguer-queen/src/App.js
+++ burguer-queen/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-//import RouterPage from "./router/Router";
-import Login from "./pages/Login";
 import AuthProvider from "./services/auth/AuthProvider";
 import AppRouter from "./router/Router";
 import './assets/styles/App.css';
