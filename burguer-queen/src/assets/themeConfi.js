09266743665ec87b45c7520d4bc8ca02e3835f8c
Claudi4 <clau.ale.20a@gmail.com>
2021-10-19 00:49:42 -0500

    change in login color

---
+++ burguer-queen/src/assets/themeConfi.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#F8A000',
+        },
+        secondary: {
+            main: '#f44336',
+        },
+    },
+});
--- burguer-queen/src/pages/Login.js
+++ burguer-queen/src/pages/Login.js
@@ -1,11 +1,13 @@
-import React,{useState} from "react";
-import { Grid, Paper, Container , Avatar, Typography, TextField, Button,CssBaseline} from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, Paper, Container, Typography, TextField, Button, CssBaseline } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import fondo from '../assets/img/fondoHamburguesas.jpg';
+import fondo from '../assets/img/fondo4.png';
 import logo from '../assets/img/Logo.svg';
-import {LockOutlined as LockOutlinedIcon} from '@material-ui/icons'
+//import {LockOutlined as LockOutlinedIcon} from '@material-ui/icons'
 import { useHistory, useLocation } from 'react-router-dom';
 import useAuth from "../services/auth/useAuth";
+
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -17,37 +19,38 @@ const useStyles = makeStyles(theme => ({
         height: '100vh'
     },
     container: {
-        opacity:'0.8',
-        height: '60%',
+        opacity: '0.8',
         marginTop: theme.spacing(10),
-        [theme.breakpoints.down(400 + theme.spacing(2)+2)]: {
-        marginTop: 0,
-        with: '100%',
-        height: '100%'
+        [theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
+            marginTop: 0,
+            with: '100%',
+            height: '100%'
         }
     },
-    div:{
+    div: {
         marginTop: theme.spacing(8),
-        display:'flex',
+        display: 'flex',
         flexDirection: 'column',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    avatar:{
+    avatar: {
         margin: theme.spacing(1),
         backgroundColor: theme.palette.primary.main
     },
-    form:{
-        width:'100%',
+    form: {
+        width: '100%',
         marginTop: theme.spacing(1)
     },
-    button:{
-        margin: theme.spacing(3,0,2)
+    button: {
+        margin: theme.spacing(3, 0, 2),
+        marginBottom: '3rem',
+        backgroundColor: theme.palette.primary.main
     }
 
 }))
 
 const Login = () => {
-    const[body,setBody] =useState({nickname:'',password:''})
+    const [body, setBody] = useState({ nickname: '', password: '' })
     const classes = useStyles()
 
     const history = useHistory();
@@ -58,19 +61,75 @@ const Login = () => {
     const handleChange = e => {
         setBody({
             ...body,
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
-    const onSubmit=()=>{
+    const onSubmit = () => {
         console.log(body);
-      // @parameters email, password
-      auth.login('amelanie.trillo27@gmail.com','Melanie#27');
-      history.push(previusObjectURL || "/admin")
+        // @parameters email, password
+        auth.login('amelanie.trillo27@gmail.com', 'Melanie#27');
+        history.push(previusObjectURL || "/admin")
     }
 
 
     return (
+        <div>
+            <Grid container component="main" className={classes.root}>
+                <Grid item xs={12} sm={6}>
+                    <img src={fondo} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='brand' />
+                </Grid>
+                <CssBaseline />
+                <Grid item xs={12} sm={6} style={{ padding: 10 }}>
+                    <Container component={Paper} elevation={5} maxWidth='xs' className={classes.container}>
+                        <div className={classes.div}>
+                            <Grid container justifyContent='center'>
+                                <img src={logo} style={{ width: 200, height: 180 }} alt='logo' />
+                            </Grid>
+                            <Typography component='h1' variant='h5'>Sign In</Typography>
+                            <form className={classes.from}>
+                                <TextField
+                                    fullWidth
+                                    autoFocus
+                                    color='primary'
+                                    margin='normal'
+                                    variant='outlined'
+                                    label='Nickname'
+                                    name='nickname'
+                                    value={body.nickname}
+                                    onChange={handleChange}
+                                />
+                                <TextField
+                                    fullWidth
+                                    type='password'
+                                    color='primary'
+                                    margin='normal'
+                                    variant='outlined'
+                                    label='Password'
+                                    name='password'
+                                    value={body.password}
+                                    onChange={handleChange}
+                                />
+                                <Button
+                                    fullWidth
+                                    //color='primary'
+                                    variant='contained'
+                                    className={classes.button}
+                                    onClick={() => onSubmit()}
+                                >
+                                    Sign In
+                                </Button>
+                            </form>
+                        </div>
+                    </Container>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+export default Login;
+
+/*return (
         <div>
             <Grid container component="main" className={classes.root}>
                 <Grid item xs={12 } sm={6}>
@@ -80,12 +139,12 @@ const Login = () => {
                 <Grid item xs={12 } sm={6} style={{padding:10}}>
                     <Grid container justifyContent='center'>
                     <img src={logo} style={{width:200}} alt='logo'/>
-                    
+
                 </Grid>
                 <Container component={Paper} elevation={5} maxWidth='xs' className= {classes.container}>
                     <div className={classes.div}>
                         <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon/>   
+                        <LockOutlinedIcon/>
                         </Avatar>
                         <Typography component = 'h1' variant = 'h5'>Sign In</Typography>
                         <form className={classes.from}>
@@ -128,4 +187,4 @@ const Login = () => {
         </div>
     )
 }
-export default Login;
+export default Login;*/
